4ce02c9158bbe027daf24e9fab928a2357c55937
louvick <louvick78@yahoo.com>
2023-01-31 14:34:38 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,42 +5,42 @@ echo "Paramètres reçus:<br/><pre>";
 echo "</pre>----------------------------<br/>";
 session_start();
 //Est-ce qu'un paramètre action est présent
-if (isset($_GET['action'])) {
+if (isset($_REQUEST['action'])) {
     
     //Est-ce que l'action demandée est la liste des produits
-    if ($_GET['action'] == 'produits') {
+    if ($_REQUEST['action'] == 'produits') {
         //Ajoute le controleur de Produit
         require('controller/controllerProduit.php');
         //Appel la fonction listProduits contenu dans le controleur de Produit
         listProduits();
     }
     // Sinon est-ce que l'action demandée est la description d'un produit
-    elseif ($_GET['action'] == 'produit') {
+    elseif ($_REQUEST['action'] == 'produit') {
         
         // Est-ce qu'il y a un id en paramètre
-        if (isset($_GET['id']) && $_GET['id'] > 0) {
+        if (isset($_REQUEST['id']) && $_REQUEST['id'] > 0) {
             //Ajoute le controleur de Produit
             require('controller/controllerProduit.php');
             //Appel la fonction produit contenu dans le controleur de Produit
-            produit($_GET['id']);
+            produit($_REQUEST['id']);
         }
         else {
             //Si on n'a pas reçu de paramètre id, mais que la page produit a été appelé
             echo 'Erreur : aucun identifiant de produit envoyé';
         }
     } 
-    elseif ($_GET['action'] == 'categories') {
+    elseif ($_REQUEST['action'] == 'categories') {
         require('controller/controllerCategorie.php');
         listCategories();
     }
-    elseif ($_GET['action'] == 'produitscategorie') {
-        if (isset($_GET['id']) && $_GET['id'] > 0) {
+    elseif ($_REQUEST['action'] == 'produitscategorie') {
+        if (isset($_REQUEST['id']) && $_REQUEST['id'] > 0) {
             require('controller/controllerProduit.php');
 
-            listProduitsCategorie($_GET['id']);
+            listProduitsCategorie($_REQUEST['id']);
         }
     }
-    elseif ($_GET['action'] == 'connexion') {
+    elseif ($_REQUEST['action'] == 'connexion') {
         require('controller/controllerUtilisateur.php');
         getFormConnexion();
     }
