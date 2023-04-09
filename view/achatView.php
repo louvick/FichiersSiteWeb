adce59831e2bb21eb36dcaf58824d0d45ed4120f
louvick <60804086+louvick@users.noreply.github.com>
2023-04-09 13:05:56 -0400

    f

---
+++ view/achatView.php
@@ -0,0 +1,8 @@
+<div id="paypal-button-container">
+    <div class="grid-container">
+      <div class="grid-item item1">Column 1</div>
+      <div class="grid-item item2">Column 2</div>
+      <div class="grid-item item3">Column 3</div>
+      <div class="grid-item item4">Column 4</div>
+    </div>
+</div>
